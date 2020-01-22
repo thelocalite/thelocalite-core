b272eee395eb47fd56e7770b61d4ed69ec6739eb
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-22 12:56:23 +0530

    Add comments for Lombok Annotations

--- src/main/java/core/model/ProductVendor.java
+++ src/main/java/core/model/ProductVendor.java
@@ -5,6 +5,11 @@ import java.io.Serializable;
 import javax.persistence.*;
 import lombok.*;
 
+/**
+ * Generates getters for all fields, a useful toString method, and hashCode and
+ * equals implementations that check all non-transient fields. Will also
+ * generate setters for all non-final fields, as well as a constructor.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/core/model/Vendor.java
+++ src/main/java/core/model/Vendor.java
@@ -6,6 +6,11 @@ import java.util.Set;
 import javax.persistence.*;
 import lombok.*;
 
+/**
+ * Generates getters for all fields, a useful toString method, and hashCode and
+ * equals implementations that check all non-transient fields. Will also
+ * generate setters for all non-final fields, as well as a constructor.
+ */
 @Data
 @NoArgsConstructor
 
