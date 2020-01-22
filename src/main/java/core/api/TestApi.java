40b8cf31cf9985092820d510da9f7922d0aa081b
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-22 19:58:17 +0530

    Separate Product APIs in ProductRestController

--- src/main/java/core/api/TestApi.java
+++ src/main/java/core/api/TestApi.java
@@ -1,7 +1,5 @@
 package core.api;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,35 +30,4 @@ public class TestApi {
 	public void deleteProduct(@PathVariable("productName") String productName) {
 		productService.deleteProductByName(productName);
 	}
-
-	// Add product using POST parameters from Angular
-	@PostMapping(path = "/product/add")
-	public @ResponseBody String addNewProduct(@RequestBody Product product) {
-		Product product1 = new Product();
-		product1.setProductName(product.getProductName());
-		product1.setProductDescription(product.getProductDescription());
-		product1.setProductCategory(product.getProductCategory());
-		product1.setProductMrp(product.getProductMrp());
-		product1.setProductImageUrl(product.getProductImageUrl());
-		product1.setProductBrand(product.getProductBrand());
-		productService.addProduct(product1);
-		return "{\"message\":\"Added new Product\"}";
-	}
-
-	// Gets all product entities as JSON
-	@GetMapping(path = "/product")
-	public @ResponseBody Iterable<Product> getAllProducts() {
-		return productService.getAllProducts();
-	}
-
-	// Gets product by id
-	@GetMapping(path = "/product/{id}")
-	public @ResponseBody Product getProduct(@PathVariable("id") Long id) {
-		Optional<Product> product = productService.getProduct(id);
-		if (product.isPresent())
-			return product.get();
-		else
-			return null;
-	}
-
 }
