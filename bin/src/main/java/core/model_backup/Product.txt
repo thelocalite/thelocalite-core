ac8238333d91607996c51ff026df1794995d57c4
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-23 10:40:35 +0530

    deploy configuration

---
+++ bin/src/main/java/core/model_backup/Product.txt
@@ -0,0 +1,78 @@
+package core.model_backup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String description;
+	private String imgUrl;
+
+	@OneToMany(mappedBy = "product")
+	private Set<ProductVendor> productVendors = new HashSet<>();
+
+	// Default constructor
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+	// Parameterised constructor
+	public Product(String name) {
+		super();
+		this.name = name;
+	}
+
+	// Getters & Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<ProductVendor> getProductVendors() {
+		return productVendors;
+	}
+
+	public void setProductVendors(Set<ProductVendor> productVendors) {
+		this.productVendors = productVendors;
+	}
+
+}
--- src/main/java/core/api/ProductRestController.java
+++ src/main/java/core/api/ProductRestController.java
@@ -1,12 +1,10 @@
 package core.api;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import core.model.Product;
 import core.service.ProductService;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 // Enable Cross-Origin Resource Sharing for GET request
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
@@ -16,37 +14,35 @@ import core.service.ProductService;
 @RequestMapping("/product")
 public class ProductRestController {
 
-    @Autowired
-    ProductService productService;
+  @Autowired ProductService productService;
 
-    // Add product using POST parameters from Angular
-    @PostMapping(path = "/add")
-    public @ResponseBody String addNewProduct(@RequestBody Product product) {
-        Product product1 = new Product();
-        product1.setProductName(product.getProductName());
-        product1.setProductDescription(product.getProductDescription());
-        product1.setProductCategory(product.getProductCategory());
-        product1.setProductMrp(product.getProductMrp());
-        product1.setProductImageUrl(product.getProductImageUrl());
-        product1.setProductBrand(product.getProductBrand());
-        productService.addProduct(product1);
-        return "{\"message\":\"Added new Product\"}";
-    }
+  // Add product using POST parameters from Angular
+  @PostMapping(path = "/add")
+  public @ResponseBody String addNewProduct(@RequestBody Product product) {
+    Product product1 = new Product();
+    product1.setProductName(product.getProductName());
+    product1.setProductDescription(product.getProductDescription());
+    product1.setProductCategory(product.getProductCategory());
+    product1.setProductMrp(product.getProductMrp());
+    product1.setProductImageUrl(product.getProductImageUrl());
+    product1.setProductBrand(product.getProductBrand());
+    productService.addProduct(product1);
+    return "{\"message\":\"Added new Product\"}";
+  }
 
-    // Gets all product entities as JSON Array
-    @GetMapping(path = "")
-    public @ResponseBody Iterable<Product> getAllProducts() {
-        return productService.getAllProducts();
-    }
+  // Gets all product entities as JSON Array
+  @GetMapping(path = "")
+  public @ResponseBody Iterable<Product> getAllProducts() {
+    return productService.getAllProducts();
+  }
 
-    // Gets product by id
-    @GetMapping(path = "/{id}")
-    public @ResponseBody Product getProduct(@PathVariable("id") Long id) {
-        Optional<Product> product = productService.getProduct(id);
-        if (product.isPresent())
-            return product.get();
-        else
-            return null;
-    }
-
+  // Gets product by id
+  @GetMapping(path = "/{id}")
+  public @ResponseBody Product getProduct(@PathVariable("id") Long id) {
+    Optional<Product> product = productService.getProduct(id);
+    if (product.isPresent())
+      return product.get();
+    else
+      return null;
+  }
 }
