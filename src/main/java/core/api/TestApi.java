44862ed8b7c9bbaace86f6342c801835244e8daf
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-23 18:02:06 +0530

    test 4

--- src/main/java/core/api/TestApi.java
+++ src/main/java/core/api/TestApi.java
@@ -1,11 +1,10 @@
 package core.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import core.model.Product;
 import core.service.ProductService;
 import core.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 // Enable Cross-Origin Resource Sharing for GET request
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
@@ -15,19 +14,22 @@ import core.service.TestService;
 @RequestMapping("/")
 public class TestApi {
 
-	@Autowired
-	TestService testService;
+  @Autowired TestService testService;
 
-	@Autowired
-	ProductService productService;
+  @Autowired ProductService productService;
 
-	@RequestMapping("/addProduct/{productName}")
-	public void addProduct(@PathVariable("productName") String productName) {
-		productService.addProduct(new Product(productName));
-	}
+  @RequestMapping("")
+  public String hello() {
+    return "{\"message\":\"Hello\"}";
+  }
 
-	@RequestMapping("deleteProduct/{productName}")
-	public void deleteProduct(@PathVariable("productName") String productName) {
-		productService.deleteProductByName(productName);
-	}
+  @RequestMapping("/addProduct/{productName}")
+  public void addProduct(@PathVariable("productName") String productName) {
+    productService.addProduct(new Product(productName));
+  }
+
+  @RequestMapping("deleteProduct/{productName}")
+  public void deleteProduct(@PathVariable("productName") String productName) {
+    productService.deleteProductByName(productName);
+  }
 }
