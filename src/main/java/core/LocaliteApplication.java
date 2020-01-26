f062faf593ec27bd2e6d60ff76ed5ef7ee174acf
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-26 14:55:38 +0530

    comment out commandline runner as a suspect for unexpected application shutdowns due to MainClassNotFound Errors

--- src/main/java/core/LocaliteApplication.java
+++ src/main/java/core/LocaliteApplication.java
@@ -1,63 +1,62 @@
 package core;
 
-import javax.transaction.Transactional;
+// import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import core.model.products.Product;
-import core.model.products.ProductVendor;
-import core.model.products.Vendor;
-import core.repository.products.ProductRepository;
-import core.repository.products.VendorRepository;
+// import core.model.products.Product;
+// import core.model.products.ProductVendor;
+// import core.model.products.Vendor;
+// import core.repository.products.ProductRepository;
+// import core.repository.products.VendorRepository;
 
 @SpringBootApplication
-public class LocaliteApplication implements CommandLineRunner {
+// public class LocaliteApplication implements CommandLineRunner {
+public class LocaliteApplication {
 
-  @Autowired
-  private ProductRepository productRepository;
+  // @Autowired
+  // private ProductRepository productRepository;
 
-  @Autowired
-  private VendorRepository vendorRepository;
+  // @Autowired
+  // private VendorRepository vendorRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(LocaliteApplication.class, args);
   }
 
-  @Override
-  @Transactional
-  public void run(String... args) throws Exception {
-    prodTest();
-  }
+  // @Override
+  // @Transactional
+  // public void run(String... args) throws Exception {
+  // prodTest();
+  // }
 
-  public void prodTest() {
-
-    Product prodA = new Product("Maggi Atta Noodles", "Nestle", "Foods", "An awesome product by maggi", 20.0,
-        "imageUrl");
-
-    Vendor vendor1 = new Vendor("name1", "desc1", "url1");
-    Vendor vendor2 = new Vendor("name2", "desc2", "url2");
-
-    ProductVendor prodVendX = new ProductVendor();
-    prodVendX.setProduct(prodA);
-    prodVendX.setVendor(vendor1);
-    prodVendX.setVendorSpecificPrice(20.0);
-
-    prodA.getProductVendors().add(prodVendX);
-
-    ProductVendor prodVendY = new ProductVendor();
-    prodVendY.setProduct(prodA);
-    prodVendY.setVendor(vendor2);
-    prodVendY.setVendorSpecificPrice(110.0);
-
-    prodA.getProductVendors().add(prodVendY);
-
-    vendorRepository.save(vendor1);
-    vendorRepository.save(vendor2);
-    productRepository.save(prodA);
-
-  }
+  /*
+   * 
+   * public void prodTest() {
+   * 
+   * Product prodA = new Product("Maggi Atta Noodles", "Nestle", "Foods",
+   * "An awesome product by maggi", 20.0, "imageUrl");
+   * 
+   * Vendor vendor1 = new Vendor("name1", "desc1", "url1"); Vendor vendor2 = new
+   * Vendor("name2", "desc2", "url2");
+   * 
+   * ProductVendor prodVendX = new ProductVendor(); prodVendX.setProduct(prodA);
+   * prodVendX.setVendor(vendor1); prodVendX.setVendorSpecificPrice(20.0);
+   * 
+   * prodA.getProductVendors().add(prodVendX);
+   * 
+   * ProductVendor prodVendY = new ProductVendor(); prodVendY.setProduct(prodA);
+   * prodVendY.setVendor(vendor2); prodVendY.setVendorSpecificPrice(110.0);
+   * 
+   * prodA.getProductVendors().add(prodVendY);
+   * 
+   * vendorRepository.save(vendor1); vendorRepository.save(vendor2);
+   * productRepository.save(prodA);
+   * 
+   * }
+   */
 
 }
