86f75537aff6b4031ed9f9dbae5424f499b59deb
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-28 18:14:22 +0530

    Make Search Even Better  :open_mouth:
    How do you make search better you ask?
    
    Well do I have an answer for you!
    
    The first iteration of search used Substring Logic which was good at partial matches but was unforgiving in terms of spelling errors
    
    The second iteration changed that with Soundex Logic. It was now possible to make spelling mistakes and still get the correct product as long as the search term sounded like the product name. But this implementation did not consider substrings
    
    This third iteration of Search integrates the Substring Logic with the Soundex Logic making search so much better.

--- src/main/java/core/api/ProductAPI.java
+++ src/main/java/core/api/ProductAPI.java
@@ -56,31 +56,56 @@ public class ProductAPI {
     }
 
     /**
-     * Smart* Search API
+     * @technologic808
      * 
-     * Takes in query for product and returns search results
+     *                 Smart* Search API
      * 
-     * Now Implements Soundex :D
+     *                 Takes in query for product and returns search results
      * 
-     * Soundex is an algorithm invented 100 years ago (between 1918-1922) and used
-     * to relate similar sounding names
+     *                 Now Implements Soundex :D
      * 
-     * Uses the Apache Commons Codec Library to import the encoding functionality
+     *                 Soundex is an algorithm invented 100 years ago (between
+     *                 1918-1922) and used to relate similar sounding names
      * 
-     * The Soundex object encodes a string into a Soundex value which is then
-     * compared to another string encoded into a Soundex value using the equals()
-     * and encode() method
+     *                 Uses the Apache Commons Codec Library to import the encoding
+     *                 functionality
      * 
-     * Since the strings in the search term and the product name could be more than
-     * one word, we break up the strings into individual words using split() method
-     * of String object We iterate through every combination and add the product to
-     * the Collection if matched
+     *                 The Soundex object encodes a string into a Soundex value
+     *                 which is then compared to another string encoded into a
+     *                 Soundex value using the equals() and encode() method
      * 
-     * Another issue was that during the iteration, multiple matches were being
-     * found, so the arraylist was being loaded with duplicate product objects. The
-     * Solution was to use a Set instead of a List
+     *                 Since the strings in the search term and the product name
+     *                 could be more than one word, we break up the strings into
+     *                 individual words using split() method of String object We
+     *                 iterate through every combination and add the product to the
+     *                 Collection if matched
      * 
-     * The API now returns a HashSet instead of an ArrayList
+     *                 Another issue was that during the iteration, multiple matches
+     *                 were being found, so the arraylist was being loaded with
+     *                 duplicate product objects. The Solution was to use a Set
+     *                 instead of a List
+     * 
+     *                 The API now returns a HashSet instead of an ArrayList
+     * 
+     *                 Iteration 3: Make Search Even Better
+     * 
+     *                 How do you make search better you ask?
+     * 
+     *                 Well do I have an answer for you!
+     * 
+     *                 The first iteration of search used Substring Logic which was
+     *                 good at partial matches but was unforgiving in terms of
+     *                 spelling errors.
+     * 
+     *                 The second iteration changed that with Soundex Logic. It was
+     *                 now possible to make spelling mistakes and still get the
+     *                 correct product as long as the search term sounded like the
+     *                 product name. But this implementation did not consider
+     *                 substrings.
+     * 
+     *                 This third iteration of Search integrates the Substring Logic
+     *                 with the Soundex Logic making search so much better.
+     * 
      */
 
     @GetMapping("/search/{searchTerm}")
@@ -92,9 +117,11 @@ public class ProductAPI {
         // Search Logic
         List<Product> allProducts = productVendorService.getAllProducts();
 
-        // Soundex Logic
+        // Soundex Logic + SubString Logic = Better Search
         Soundex soundex = new Soundex();
         for (Product product : allProducts) {
+            if (product.getName().toLowerCase().contains(searchTerm.toLowerCase()))
+                products.add(product);
             for (String productword : product.getName().split("\\s+")) {
                 for (String searchTermword : searchTerm.split("\\s+")) {
                     if (soundex.encode(productword).equals(soundex.encode(searchTermword)))
