079930d48fba3a8cf6233a23b5d96762363faa04
technologic <20627603+technologic808@users.noreply.github.com>
2020-02-01 01:41:45 +0530

    Organize Imports

--- src/main/java/core/repository/products/ProductRepository.java
+++ src/main/java/core/repository/products/ProductRepository.java
@@ -1,7 +1,6 @@
 package core.repository.products;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
