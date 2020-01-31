91e90647626cc53563c49c636a8ffbe886701dce
Vijaya Aditya <vatadepalli@hotmail.com>
2020-02-01 04:27:33 +0530

    crossorigin to user

--- src/main/java/core/security/controllers/UserAPI.java
+++ src/main/java/core/security/controllers/UserAPI.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import core.security.data.AuthRepository;
@@ -18,6 +19,7 @@ import core.security.models.Auth;
  */
 @RestController
 @RequestMapping("/user")
+@CrossOrigin  
 public class UserAPI {
 
     @Autowired
